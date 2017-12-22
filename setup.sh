c09fdd94f7bac4181b11e5e04f67cbf2388121ae
miya-kei <none>
2017-12-23 03:39:01 +0900

    simplified symbolic link

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export dotfiles_dir=$HOME/dotfiles
+export dot_dir=$HOME/dotfiles
 
 cd $HOME/dotfiles
 
@@ -8,28 +8,23 @@ cd $HOME/dotfiles
 for file in .??*; do
 	[[ "$file" == ".git" ]] && continue
 	[[ "$file" == ".DS_Store" ]] && continue
+	[[ "$file" == ".gitignore" ]] && continue
+	[[ "$file" == ".gitmodules" ]] && continue
 	[[ "$file" == ".??*" ]] && continue
 
 	# check existing files
-	if [ -f "$HOME/${file}" ]; then
+	if [ ! -L "$HOME/${file}" ]; then
 		echo "Already has ${file}. Backup for the existing ${file} ? [Y/y/N/n]"
 		read ANSWER
 		case $ANSWER in
 			"Y" | "y" | "yes" | "Yes" | "YES" )
-				if [ -L $HOME/${file} ]; then
-					echo "The ${file} is a symbolic link."
-				else
-					mv $HOME/${file} $HOME/${file}_bak
-					echo "Backed up the existing ${file} as ${file}_bak"
-				fi
+				echo "Backed up the existing ${file} as ${file}_bak"
+				mv $HOME/${file} $HOME/${file}_bak
 				;;
 			* )
-				echo "Check your existing ${file}.";;
 		esac
-	else
-		ln -sf ${dotfiles_dir}/${file} $HOME/${file}
-		echo "Created symbolic link for ${file}"
 	fi
+	ln -sfnv ${dot_dir}/${file} $HOME/${file}
 done
 
 source $HOME/.bashrc
